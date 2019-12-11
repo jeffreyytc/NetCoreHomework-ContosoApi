7ac28cf033a2cfd63a67ca1eb18ccf6026bf5c3a
jeff <jeffrey.ytc@gmail.com>
2019-12-11 21:00:57 +0800

    enrollments controller

---
+++ Controllers/EnrollmentsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ContosoApi.Models;
+
+namespace ContosoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        private readonly ContosouniversityContext _context;
+
+        public EnrollmentsController(ContosouniversityContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Enrollments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Enrollment>>> GetEnrollments()
+        {
+            return await _context.Enrollments.ToListAsync();
+        }
+
+        // GET: api/Enrollments/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEnrollment(int id)
+        {
+            var enrollment = await _context.Enrollments.Where(e => e.EnrollmentId == id).Select(e => new
+            {
+                Id = e.EnrollmentId,
+                Course = new
+                {
+                    Id = e.CourseId,
+                    e.Course.Title
+                },
+                Student = new {
+                    Id = e.StudentId,
+                    FullName = $"{e.Student.FirstName} {e.Student.LastName}"
+                },
+                e.Grade
+            }).FirstOrDefaultAsync();
+
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+
+            return new JsonResult(enrollment);
+        }
+
+        // PUT: api/Enrollments/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEnrollment(int id, Enrollment enrollment)
+        {
+            if (id != enrollment.EnrollmentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(enrollment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EnrollmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Enrollments
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Enrollment>> PostEnrollment(Enrollment enrollment)
+        {
+            _context.Enrollments.Add(enrollment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEnrollment", new { id = enrollment.EnrollmentId }, enrollment);
+        }
+
+        // DELETE: api/Enrollments/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Enrollment>> DeleteEnrollment(int id)
+        {
+            var enrollment = await _context.Enrollments.FindAsync(id);
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Enrollments.Remove(enrollment);
+            await _context.SaveChangesAsync();
+
+            return enrollment;
+        }
+
+        private bool EnrollmentExists(int id)
+        {
+            return _context.Enrollments.Any(e => e.EnrollmentId == id);
+        }
+    }
+}
